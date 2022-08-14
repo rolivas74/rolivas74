645ba11f98a21a96fd589c6d0bfcb8c50a8f5380
Ricardo Olivas <jrolivas74@gmail.com>
2022-08-13 23:25:09 -0700

    primer commit

---
+++ apiJuguetes/Controllers/JuguetesController.cs
@@ -0,0 +1,126 @@
+﻿using apiJuguetes.Context;
+using apiJuguetes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiJuguetes.Controllers
+{
+    //[Route("api/[Juguetes]")]
+    [ApiController]
+    public class JuguetesController : ControllerBase
+    {
+        private readonly AppJuguetesContext context;
+        public JuguetesController(AppJuguetesContext _context)
+        {
+            this.context = _context;
+        }
+
+        //GET api/<controller>
+        [HttpGet]
+        [Route("Juguetes/Get")]
+        public ActionResult Get()
+        {
+            try
+            {
+                
+                return Ok(context.juguetes.ToList());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // GET api/<controllers>/7
+        [HttpGet]
+        [Route("Juguetes/Get/{id}")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var juguete = context.juguetes.FirstOrDefault(x => x.Id == id);
+                return Ok(juguete);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("Juguetes/Post")]
+        public ActionResult Post([FromBody]Juguetes juguete)
+        {
+            try
+            {
+                context.juguetes.Add(juguete);
+                context.SaveChanges();
+                var last = context.juguetes.OrderByDescending(i=> i.Id).FirstOrDefault();
+                return Ok(last);
+                //return CreatedAtRoute("Get", new { id = juguete.Id }, juguete);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // PUT api/<controller>/7
+        [HttpPut("id")]
+        [Route("Juguetes/Put/{id}")]
+        public ActionResult Put(int id, [FromBody] Juguetes juguete)
+        {
+            try
+            {
+                if (juguete.Id == id)
+                {
+                    context.Entry(juguete).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return Ok(id);
+                    //return CreatedAtRoute("GetJuguete", new { id = juguete.Id }, juguete);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE api/<controller>/7
+        [HttpDelete("id")]
+        [Route("Juguetes/Delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var juguete = context.juguetes.FirstOrDefault(x => x.Id == id);
+                if (juguete != null)
+                {
+                    context.juguetes.Remove(juguete);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+       
+    }
+}
---
+++ apiJuguetes/Startup.cs
@@ -0,0 +1,76 @@
+using apiJuguetes.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiJuguetes
+{
+    public class Startup
+    {
+        private readonly string _MyCors = "MyCors";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<AppJuguetesContext>(option =>
+              option.UseSqlServer(Configuration.GetConnectionString("Conection")));
+            services.AddControllersWithViews();
+            // permisos para consumir apis
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: _MyCors, builder =>
+                {
+                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                    .AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            // permisos para consumir apis
+            app.UseCors(_MyCors);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
