1e103adc2697cfab5f8ac38704936473209e3c31
Ricardo Olivas <jrolivas74@gmail.com>
2022-08-14 01:31:32 -0700

    segundo commit

---
+++ apiJuguetes/Context/AppJuguetesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiJuguetes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiJuguetes.Context
+{
+    public class AppJuguetesContext: DbContext
+    {
+        public AppJuguetesContext(DbContextOptions<AppJuguetesContext> options): base(options) 
+        {
+
+        }
+        public DbSet<Juguetes> juguetes { get; set; }
+    }
+}
---
+++ apiJuguetes/Models/Juguetes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiJuguetes.Models
+{
+    public class Juguetes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Nombre { get; set; }
+        [StringLength(100)]
+        public string Descripcion { get; set; }
+        [Range(0,100)]
+        public int RestriccionEdad { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Compania { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public decimal Precio { get; set; }
+
+    }
+}
